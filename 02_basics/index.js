b3766956df7666946354a20c8eff2a22df905f53
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2025-01-27 18:00:57 +0530

    Create index.js

---
+++ 02_basics/index.js
@@ -0,0 +1,54 @@
+// conditionals in JS //
+
+
+let age = 12 ;
+let gender = "male" ;
+
+if ( age >= 18 && gender == "male") {
+  console.log("You are a adult male");
+}
+
+else {
+  console.log("You are not a adult male");
+}
+
+let age2 = 12 ;
+let gender2 = "female" ;
+
+if ( age2 >= 18 ||gender2 == "female") {
+  console.log("You are a adult female");
+}
+else {
+  console.log("You are not a adult female");
+}
+
+let number = 5 ;
+if ( number %2 != 0) {
+  console.log("odd");
+}
+else {
+  console.log("even");
+}
+//Loops in JS//
+
+for (let i = 1 ; i <= 10 ; i++ ) {
+  console.log("Saptarshi");
+}
+
+let i = 10 ;
+let j = 20 ;
+while( i!=j) {
+  console.log("Anuska");
+  i++ ;
+}
+
+
+
+
+let m = 10 ;
+let n = 12 ;
+do {
+  console.log("Anurshi");
+  m++ ;
+} while(m<=n);
+
